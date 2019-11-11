c1d74784bd3ba3a8ab23ce050c03b037e6c14a6d
passion <563129582@qq.com>
2019-11-11 14:54:31 +0800

    fist commit

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+public class Demo : MonoBehaviour
+{
+    public static string url = "http://192.168.4.134:8888/tripartite/policy/1";
+
+    void Start()
+    {
+        TestDownload();
+    }
+
+    public static void TestUpload()
+    {
+        //1.请求Token，添加header验证
+        Dictionary<string, string> form = new Dictionary<string, string>();
+        form.Add("Authorization", "Bearer ffadb808-8c3d-4e82-a1f5-4dd02bc2477a");
+        SimpleHttp.HttpPost(url, form, null, (WWW wwwInfo) =>
+        {
+            Debug.Log(wwwInfo.text);
+            /**{
+                "code": 200, 
+                "message": "success", 
+                "data": {
+                    "token": "xxxxxxxxxxxx"
+                }, 
+                "timestamp": "1573436429382"
+            }
+            */
+            JObject root = JObject.Parse(wwwInfo.text);
+            int code = Convert.ToInt32(root["code"]);
+            if (code == 200)
+            {
+                //2.获得token
+                string token = Convert.ToString(root["data"]["token"]);
+                string key = "TestImage";
+                string filePath = "D:/TestImage.jpg";
+
+                QiniuUtil.UploadFile(key, filePath, token, (string text) => {
+                    Debug.Log(text);
+                });
+            }
+        });
+    }
+
+    public static void TestDownload()
+    {
+        string url = "http://static.ingoing.company.cn/TestImage";
+        QiniuUtil.DownloadFile(url, "D:/download.png", (int code) => {
+            Debug.Log(code);
+        });
+    }
+}
---
+++ Assets/Scripts/QiniuUtil.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using Qiniu.Http;
+using Qiniu.Storage;
+
+public class QiniuUtil
+{
+    //上传本地文件
+    public static void UploadFile(string key, string filePath, string token, Action<string> callback)
+    {
+        //配置config
+        Config config = new Config();
+        config.Zone = Zone.ZONE_CN_South;
+        config.UseHttps = false;
+        config.UseCdnDomains = false;
+        config.ChunkSize = ChunkUnit.U512K;
+
+        //>表单上传
+        UploadManager target = new UploadManager(config);
+        HttpResult result = target.UploadFile(filePath, key, token, null);
+        if (callback != null)
+            callback(result.Text);
+    }
+
+    //上传字节流
+    public static void UploadData(string key, byte[] data, string token, Action<string> callback)
+    {
+        //byte[] data = System.IO.File.ReadAllBytes(filePath);
+
+
+        //配置config
+        Config config = new Config();
+        config.Zone = Zone.ZONE_CN_South;
+        config.UseHttps = false;
+        config.UseCdnDomains = false;
+        config.ChunkSize = ChunkUnit.U512K;
+
+        //>表单上传
+        UploadManager target = new UploadManager(config);
+        HttpResult result = target.UploadData(data, key, token, null);
+        if (callback != null)
+            callback(result.Text);
+    }
+
+    //断点续传
+    public static void UploadFileResum(string key, string filePath, string token, Action<string> callback)
+    {
+        //配置config
+        Config config = new Config();
+        config.Zone = Zone.ZONE_CN_South;
+        config.UseHttps = false;
+        config.UseCdnDomains = false;
+        config.ChunkSize = ChunkUnit.U512K;
+
+        PutExtra put = new PutExtra();
+        put.ProgressHandler = (long uploadedBytes, long totalBytes) =>
+        {
+            Debug.Log(uploadedBytes+"/"+ totalBytes);
+        };
+
+        ResumableUploader ru = new ResumableUploader(config);
+        var result = ru.UploadFile(filePath, key, token, put);
+        if (callback != null)
+            callback(result.Text);
+    }
+
+    //下载文件
+    public static void DownloadFile(string url,string savePath, Action<int> callback)
+    {
+        HttpResult result = DownloadManager.Download(url, savePath);
+
+        if (callback != null)
+            callback(result.Code);
+    }
+
+    /// <summary>
+    /// 计算文件的MD5值
+    /// </summary>
+    public static string md5file(string file)
+    {
+        try
+        {
+            FileStream fs = new FileStream(file, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(fs);
+            fs.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("md5file() fail, error:" + ex.Message);
+        }
+    }
+
+}
